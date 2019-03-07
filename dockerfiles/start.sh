fac15505653efeec4198a877afbdc69fce0f50c6
Juraj Duc <juraj.duc@docfinder.at>
2019-03-07 11:14:29 +0100

    fixed file “encoding” so it works not only on win

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -6,9 +6,8 @@ docker-compose -f docker-compose.yml up -d
 
 winpty docker-compose exec php composer install
 
-docker-compose exec php  composer install
+docker-compose exec php  compose installed
 
-
-docker-compose exec php php yii migrate/fresh --interactive=0
+docker-compose exec php yii migrate/fresh --interactive=0
 
 winpty docker-compose exec php php yii migrate/fresh --interactive=0
--- dockerfiles/stop.sh
+++ dockerfiles/stop.sh
@@ -2,4 +2,3 @@
 DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
 
 docker-compose -f docker-compose.yml stop
-
