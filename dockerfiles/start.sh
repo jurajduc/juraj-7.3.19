842468d8822fd27baaaab030946cca44e983f3d2
Juraj Duc <juraj.duc@docfinder.at>
2019-03-07 11:29:56 +0100

    typos

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -4,10 +4,7 @@ docker-compose -f docker-compose.yml build
 
 docker-compose -f docker-compose.yml up -d
 
-winpty docker-compose exec php composer install
 
-docker-compose exec php  compose installed
+docker-compose exec php  composer install
 
 docker-compose exec php yii migrate/fresh --interactive=0
-
-winpty docker-compose exec php php yii migrate/fresh --interactive=0
