257d610e6f53c12c8e794d00c124a4ca68d88636
Juraj Duc <juraj.duc@docfinder.at>
2019-03-11 07:36:11 +0100

    migration fix

--- dockerfiles/start.sh
+++ dockerfiles/start.sh
@@ -7,4 +7,4 @@ docker-compose -f docker-compose.yml up -d
 
 docker-compose exec php  composer install
 
-docker-compose exec php yii migrate/fresh --interactive=0
+docker-compose exec php ./yii migrate/fresh --interactive=0
